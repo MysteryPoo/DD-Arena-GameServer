db9f2c402e026b329e596e3ddadfa826755bf695
MysteryPoo <Matt.Thornton@knights.ucf.edu>
2020-05-27 09:56:25 -0400

    Non hardcoded versioning

--- src/Protocol/LobbyServerInterface/Handlers/Challenge.ts
+++ src/Protocol/LobbyServerInterface/Handlers/Challenge.ts
@@ -3,7 +3,7 @@ import { MessageHandlerBase } from "../../../Abstracts/MessageHandlerBase";
 import { AuthenticationChallenge } from "../Messages/Challenge";
 import { LobbyClient } from "../../../LobbyClient";
 import { Handshake } from "../Messages/Handshake";
-import { MESSAGE_ID } from "../../../LobbyConnectionManager";
+import { MESSAGE_ID, LobbyConnectionManager } from "../../../LobbyConnectionManager";
 
 export class ChallengeHandler extends MessageHandlerBase {
 
@@ -11,8 +11,9 @@ export class ChallengeHandler extends MessageHandlerBase {
         let message : AuthenticationChallenge = new AuthenticationChallenge(this.messageId, buffer);
 
         if (message.valid) {
+            let connectionManager : LobbyConnectionManager = myClient.connectionManager as LobbyConnectionManager;
             let response : Handshake = new Handshake(MESSAGE_ID.Handshake);
-            response.gameVersion = 0;
+            response.gameVersion = connectionManager.matchMakingProtocolVersion;
             response.gameServerPassword = myClient.gameServerPassword;
             response.playerIdList = [myClient.hostId];
             myClient.write(response.serialize());
